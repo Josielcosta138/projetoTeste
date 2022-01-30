1395eede54e97b9218a5fcde2e63f4098a6069e8
Josielcosta138 <cjosiel2@gmail.com>
2022-01-30 16:20:00 -0200

    Tratando Exception ( Inesperadas e Multiplas )

--- projetoTeste/src/pacoteteste/estruturaDeclaracao.java
+++ projetoTeste/src/pacoteteste/estruturaDeclaracao.java
@@ -1,13 +1,20 @@
 package pacoteteste;
 
 import java.awt.Container;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 import javax.swing.text.Position;
+
+import org.omg.CORBA.ExceptionList;
+
+import com.sun.org.apache.bcel.internal.classfile.Field;
 
 import cursojava.classesauxiliares.FuncaoAutenticacao;
 import cursojava.constantes.statusAluno;
@@ -21,6 +28,10 @@ public class estruturaDeclaracao {
 	public static void main(String[] args) {
 		
 		try {
+			
+			
+		//File fil = new File("teste.txt");
+		//Scanner scannerLerArquivos = new Scanner(fil);	
 
 		// New Aluno() é uma instancia ( Criação de Objeto ) cria na memória do Java
 		// aluno1 é uma referencia ao objeto Aluno6
@@ -53,8 +64,8 @@ public class estruturaDeclaracao {
 		// Entradas de Dados para os Atributos Aluno	
      	String nome = JOptionPane.showInputDialog(" Informe nome? "+posAlunos+"°");
      	String qtddiciplina = JOptionPane.showInputDialog(" Informe qntd diciplinas? ");
-     	/*String idade = JOptionPane.showInputDialog(" Informe idade? ");
-		String cpf = JOptionPane.showInputDialog(" Informe cpf? ");
+     	String idade = JOptionPane.showInputDialog(" Informe idade? ");
+     	/*String cpf = JOptionPane.showInputDialog(" Informe cpf? ");
 		String telefone = JOptionPane.showInputDialog(" Informe telefone? ");
 		String matricula = JOptionPane.showInputDialog(" Informe matricula? ");
 		String dataNascimento = JOptionPane.showInputDialog(" Informe dataNascimento? ");
@@ -67,8 +78,8 @@ public class estruturaDeclaracao {
 		
 		aluno1.setNome(nome);
 		aluno1.setQtddiciplina(Integer.valueOf(qtddiciplina)); // Convertendo String para Inteiro
-		/*aluno1.setIdade(Integer.valueOf(idade)); // Convertendo String para Inteiro
-		aluno1.setCpf(cpf);
+		aluno1.setIdade(Integer.valueOf(idade)); // Convertendo String para Inteiro
+		/*aluno1.setCpf(cpf);
 		aluno1.setTelefone(telefone);
 		aluno1.setDataMatricula(matricula);
 		aluno1.setDataNascimento(dataNascimento);
@@ -101,7 +112,7 @@ public class estruturaDeclaracao {
 			maps.put(statusAluno.REPROVADO, new ArrayList<Aluno>());
 		
 			
-			System.out.println(" Aqui o erro. ");
+			//System.out.println(" Aqui o erro. ");
 			alunos.add(aluno1); // Adicionando a o aluno1 a Lista de (alunos) 				
 		} 
 		 	
@@ -185,26 +196,26 @@ public class estruturaDeclaracao {
 					   Diciplina disc = aluno.getDiciplinas().get(posList);
 						System.out.println(" Diciplinas: "+disc.getDiciplina()+" nota: "+disc.getNota());
 				}
-		 		
-		 		
-		 		}
+		 	}
 		 	
-				
 		 	// if login
 		
 			} else {
-				
 						JOptionPane.showMessageDialog(null, " Senha ou Login Invalido! ");
-					
-					}
+				}
 			
-				}catch (Exception e) {
+				 // Inicio de tratamento e exibios de ( EXCEÇÕES )
+			
+				}catch (NumberFormatException e) {	 //Exceção inesperada ( Todas que encontrar )
 					
 					StringBuilder saida = new StringBuilder();
 					
 					e.printStackTrace(); // imprime erro no console.
 					
-					System.out.println(" Motivo do erro: "+ e.getMessage());
+					System.out.println(" ");
+					System.out.println("|--------------------------------------------------| ");
+					System.out.println(" ");
+					System.out.println(" Motivo do erro: "+ e.getMessage()); // Mostra motivo do Erro
 					System.out.println(" ");
 					
 					for (int i = 0; i < e.getStackTrace().length; i++) {
@@ -214,24 +225,25 @@ public class estruturaDeclaracao {
 						System.out.println(" Método:   "+e.getStackTrace()[i].getMethodName());
 						System.out.println(" Pasta:    "+e.getStackTrace()[i].getFileName());
 						System.out.println(" Linha:    "+e.getStackTrace()[i].getLineNumber());
+						System.out.println(" ");
+						System.out.println("|--------------------------------------------------| ");
 						
 							// Classes de Exception em Caixa de msg na tela
 							saida.append("\n Classe:   "+e.getStackTrace()[i].getClassName());
 							saida.append("\n Método:   "+e.getStackTrace()[i].getMethodName());
 							saida.append("\n Pasta:    "+e.getStackTrace()[i].getFileName());
 							saida.append("\n Linha:    "+e.getStackTrace()[i].getLineNumber());
-							saida.append("\n Classe da Exc:    "+e.getClass().getName());
+							//saida.append("\n Classe :    "+e.getClass().getName());
 							
-						
-						
-						
-						
 					}
 					
-					
 					JOptionPane.showMessageDialog(null, " Mensagem erro: "+ saida);
-			
-		    }
+		    }catch (NullPointerException e) {
+				e.printStackTrace();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		
 		}
 	}
 	
