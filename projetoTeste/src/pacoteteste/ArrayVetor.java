debf83658ce49bdd1cc3777c1bf340533026c1b8
Josielcosta138 <cjosiel2@gmail.com>
2022-02-07 23:08:29 -0200

    Recuperando valores dentro do Array

--- projetoTeste/src/pacoteteste/ArrayVetor.java
+++ projetoTeste/src/pacoteteste/ArrayVetor.java
@@ -6,19 +6,28 @@ public class ArrayVetor {
 		
 		/* Array pode ser de todos tipos de Objetos */
 		/* Array sempre tem que ter posições Definindas [0], [1].... */
-		double[] notas = new double[4];
+		double[] notas = new double[6];
 		  
 		notas[2] = 7.7;
 		notas[1] = 3.9;
 		notas[3] = 9.8;
 		notas[0] = 5;
+		 
+		double notaSoma = 0;
 		
 		
-			for (double pos : notas) {
+			for (int pos = 0; pos <= 5; pos++) {
 				
-			System.out.println(" Array:  "+pos);
+			System.out.println(" Posição Array "+ pos +". "+"  Notas "+ (pos +1)+"° = " +notas[pos]);
 			
 		}
+			
+			notaSoma = (notas[0] + notas[2]);
+			
+			System.out.println(" ");
+			System.out.println(" Nota somada: "+notaSoma);
+			
+			
 	}
 
 }
