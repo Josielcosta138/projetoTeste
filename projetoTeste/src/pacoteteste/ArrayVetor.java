5e9c7e6e4b025b9bb9641ca666db00022e8458c9
Josielcosta138 <cjosiel2@gmail.com>
2022-02-08 23:21:19 -0200

    Entrada e leitura de posições de Array [] e Vetor

--- projetoTeste/src/pacoteteste/ArrayVetor.java
+++ projetoTeste/src/pacoteteste/ArrayVetor.java
@@ -1,32 +1,48 @@
 package pacoteteste;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 	
 	public static void main(String[] args) {
 		
 		/* Array pode ser de todos tipos de Objetos */
 		/* Array sempre tem que ter posições Definindas [0], [1].... */
-		double[] notas = new double[6];
-		  
-		notas[2] = 7.7;
-		notas[1] = 3.9;
-		notas[3] = 9.8;
-		notas[0] = 5;
-		 
-		double notaSoma = 0;
+		
+		// INFORMANDO A QNTD DE POSIÇÕES NO ARRAY
+		String posicoes = JOptionPane.showInputDialog(" Informe a qntd de Posições? ");
+		
+			double notaSoma = 0;	
+			double[] notas = new double[Integer.parseInt(posicoes)];
+			
+			
+		  // INFORMANDO AS NOTAS NAS POSIÇÕES
+		  for (int pos = 0; pos < notas.length; pos++) {
+			
+			  String valor = JOptionPane.showInputDialog(" Informe a nota "+ ( pos +1));
+			  
+			  	notas[pos] =  Double.parseDouble(valor); 
+		}
 		
 		
-			for (int pos = 0; pos <= 5; pos++) {
+		  	// EXIBI NA TELA NOTAS E POSIÇÕES
+			for (int pos = 0; pos < notas.length; pos++) {
 				
-			System.out.println(" Posição Array "+ pos +". "+"  Notas "+ (pos +1)+"° = " +notas[pos]);
+				System.out.println(" Posição Array "+ pos +". "+" Nota "+ (pos +1)+"° = " +notas[pos]);
 			
 		}
+		
+			// SOMA NOTAS E EXIBI 
+				String poS = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
+				int notaSomar1 =  Integer.parseInt(poS);
+				
+				String poSs = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
+				int notaSomar2 =  Integer.parseInt(poSs);
 			
-			notaSoma = (notas[0] + notas[2]);
-			
-			System.out.println(" ");
-			System.out.println(" Nota somada: "+notaSoma);
-			
+						notaSoma = (notas[notaSomar1] + notas[notaSomar2]);
+				
+								System.out.println(" ");
+								System.out.println(" Nota somada: "+notaSoma);
 			
 	}
 
