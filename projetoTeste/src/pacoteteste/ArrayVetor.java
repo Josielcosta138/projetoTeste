c08f960a129ded0730e37d6b61041f64fa04d0be
Josielcosta138 <cjosiel2@gmail.com>
2022-02-09 23:16:42 -0200

    Formas diferentes de atribuiçoes de valores

--- projetoTeste/src/pacoteteste/ArrayVetor.java
+++ projetoTeste/src/pacoteteste/ArrayVetor.java
@@ -6,14 +6,37 @@ public class ArrayVetor {
 	
 	public static void main(String[] args) {
 		
+		
+			String[] nomes = new String[3];
+			String[] dadoFixo =  {"Joao@gmail.com ", "Bruna@gmail.com", "Asdrubol@gmail.com", "2022"};
+			
+			
+				nomes[2] = " Joao ";
+				nomes[1] = " Bruna ";
+				nomes[0] = " Aspdrubol ";
+		
+				// EXIBIÇÃO ARRAY DE NOMES
+				for (int pos = 0; pos < nomes.length; pos++) {
+					
+					System.out.println(" Posição do Array  "+(pos +1)+"° : Nome:"+nomes[pos]);
+				}
+				System.out.println(" ");
+				
+				// EXIBIÇÃO ARRAY DE DADOSFIXO
+				for (int j = 0; j < dadoFixo.length; j++) {
+				
+					System.out.println(" Posição do Array  "+(j +1)+"° : Dados fixos: "+dadoFixo[j]);
+				}
+				
 		/* Array pode ser de todos tipos de Objetos */
 		/* Array sempre tem que ter posições Definindas [0], [1].... */
 		
 		// INFORMANDO A QNTD DE POSIÇÕES NO ARRAY
-		String posicoes = JOptionPane.showInputDialog(" Informe a qntd de Posições? ");
+				/*
+		String Qntdposicoes = JOptionPane.showInputDialog(" Informe a qntd de Posições? ");
 		
 			double notaSoma = 0;	
-			double[] notas = new double[Integer.parseInt(posicoes)];
+			double[] notas = new double[Integer.parseInt(Qntdposicoes)];
 			
 			
 		  // INFORMANDO AS NOTAS NAS POSIÇÕES
@@ -33,17 +56,17 @@ public class ArrayVetor {
 		}
 		
 			// SOMA NOTAS E EXIBI 
-				String poS = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
-				int notaSomar1 =  Integer.parseInt(poS);
+				String pos1 = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
+				int notaSomar1 =  Integer.parseInt(pos1);
 				
-				String poSs = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
-				int notaSomar2 =  Integer.parseInt(poSs);
+				String pos2 = JOptionPane.showInputDialog(" informe a Posição para somar Notas");
+				int notaSomar2 =  Integer.parseInt(pos2);
 			
 						notaSoma = (notas[notaSomar1] + notas[notaSomar2]);
 				
 								System.out.println(" ");
 								System.out.println(" Nota somada: "+notaSoma);
-			
-	}
+			*/
+	}	
 
 }
