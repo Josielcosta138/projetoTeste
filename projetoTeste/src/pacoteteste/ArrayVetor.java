6784ddfe18b8be8314899297299659f526a23148
Josielcosta138 <cjosiel2@gmail.com>
2022-02-10 23:02:58 -0200

    add diciplina 2

--- projetoTeste/src/pacoteteste/ArrayVetor.java
+++ projetoTeste/src/pacoteteste/ArrayVetor.java
@@ -19,10 +19,17 @@ public class ArrayVetor {
 		Diciplina diciplina = new Diciplina();
 		diciplina.setDiciplina(" Curso Java");
 		
+		Diciplina diciplina2 = new Diciplina();
+		diciplina.setDiciplina(" Curso Banco Dados");
+		
 		double[] notas = {8,7.7,8,9};
 		diciplina.setNota(notas);
 		
+		double[] notas2 = {6,9,4,1};
+		
+		aluno.getDiciplinas().add(diciplina2);
 		aluno.getDiciplinas().add(diciplina);
+		
 		
 		System.out.println(" Aluno :"+aluno.getNome()+"\n"
 				+ " Escola: "+aluno.getNomeEscola()+"\n"
@@ -31,9 +38,20 @@ public class ArrayVetor {
 		for (int i = 0; i < notas.length; i++) {
 			
 			System.out.println(" Notas: "+diciplina.nota[i]);
-		
 		}
-		System.out.println(" Notas média: "+diciplina.getMediaNotas());
+		
+		System.out.println(" ");
+		
+		System.out.println(" Aluno :"+aluno.getNome()+"\n"
+				+ " Escola: "+aluno.getNomeEscola()+"\n"
+						+ " Diciplina: "+diciplina2.getDiciplina());
+		
+		for (int j = 0; j < notas2.length; j++) {
+			
+			System.out.println(" Notas 2: "+diciplina2.nota[j]);
+			
+		}
+		System.out.println(" Notas média: "+diciplina2.getMediaNotas());
 	}	
 	
 
