52a4576552bc676cef79403173e2a3af93105d9f
Josielcosta138 <cjosiel2@gmail.com>
2022-02-18 00:03:32 -0200

    Criando uma Matriz multidimensional

---
+++ projetoTeste/src/pacoteteste/Matriz.java
@@ -0,0 +1,23 @@
+package pacoteteste;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		
+		//Criando uma Matriz multidimensional
+		
+		int notas[][] = new int[3][3]; 
+		
+		notas[0][0] = 30;
+		notas[0][1] = 53;
+		notas[0][2] = 45;
+		
+		notas[1][0]	= 43;
+		
+		notas[2][1] = 333;
+		
+		
+		
+		System.out.println(notas);
+	}
+}
