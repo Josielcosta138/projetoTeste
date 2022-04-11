76b51d6ca439ce085fdee6b301f00c3563119210
Josielcosta138 <cjosiel2@gmail.com>
2022-04-10 23:15:11 -0300

    Nova API de Data E Hora a Partir do Java 8   BOLETOOO

--- projetoTeste/src/modulo_data/DatasEmJava.java
+++ projetoTeste/src/modulo_data/DatasEmJava.java
@@ -1,36 +1,44 @@
 package modulo_data;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
+import sun.security.jca.GetInstance.Instance;
 import sun.util.resources.CalendarData;
 
 public class DatasEmJava {
-	
-	public static void main(String[] args) throws ParseException {
-		
-		
-		/*============= Faixa de tempo com ChronoUnit =================*/
-		
-		//Date dataPassada = new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2022");
-		
-		long  dias = ChronoUnit.DAYS.between(LocalDate.parse("2021-02-22"), LocalDate.now());
-		System.out.println("Entre a data 2021-02-22 \nPossui "+dias+" Dias de diferença até a data atual! ");
-		System.out.println(" ");
-		
-		long  meses = ChronoUnit.MONTHS.between(LocalDate.parse("2022-02-22"), LocalDate.now());
-		System.out.println("Entre o MÊs 2021-06-22 \nPossui "+meses+" Mêses de diferença até a data atual! ");
-		System.out.println(" ");
-		
-		long  anos = ChronoUnit.YEARS.between(LocalDate.parse("1990-02-22"), LocalDate.now());
-		System.out.println("Entre o Ano 1990-02-22 \nPossui "+anos+" Anos de diferença até a data atual! ");
-		
-		
-		
-	}
 
-}
+	public static void main(String[] args) throws ParseException, InterruptedException {
+
+		/* ============= Nova API de Data E Hora a Partir do Java 8   BOLETOOO  ================= */
+
+		LocalDate dataBase = LocalDate.parse("2019-02-03");
+		
+		System.out.println("EMISSÃO DO SEU BOLETO! ");
+		System.out.println(" ");
+		for (int data = 1; data < 8; data++) {
+			dataBase = dataBase.plusMonths(1);
+			
+			System.out.println("Boleto "+data+"° mês : " 
+			+ dataBase.format(DateTimeFormatter.ofPattern("     dd-MM-yyyy")));
+		}
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		LocalDateTime dataEhoraAtual = LocalDateTime.now();
+		System.out.println("Ativação do Boleto! : "+
+		dataEhoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm.s"))); //ofPattern para formatar a Data
+		System.out.println(" ");
+	}
+} 
